ee00532c920d27d12674ee69623608fed119ceea
aaronbujatin <94673180+NotParticularly@users.noreply.github.com>
2022-06-18 15:05:51 +0800

    Add files via upload

---
+++ TeacherDepartment/TeacherDepartment/Form1.Designer.vb
@@ -0,0 +1,418 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtFullname = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.txtContact = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.cbDepartmentSub = New System.Windows.Forms.ComboBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(43, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(122, 105)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(268, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(439, 45)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Teacher Department System"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Goldenrod
+        Me.Panel1.Location = New System.Drawing.Point(276, 60)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(105, 22)
+        Me.Panel1.TabIndex = 2
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(387, 60)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 3
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Goldenrod
+        Me.Panel2.Location = New System.Drawing.Point(593, 60)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(114, 22)
+        Me.Panel2.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(96, 145)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 17)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "ID : "
+        '
+        'txtID
+        '
+        Me.txtID.Enabled = False
+        Me.txtID.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(195, 142)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(218, 25)
+        Me.txtID.TabIndex = 5
+        '
+        'txtFullname
+        '
+        Me.txtFullname.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtFullname.Location = New System.Drawing.Point(196, 175)
+        Me.txtFullname.Name = "txtFullname"
+        Me.txtFullname.Size = New System.Drawing.Size(217, 25)
+        Me.txtFullname.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(95, 178)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 17)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Fullname :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(96, 210)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 17)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Gender : "
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(196, 208)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(55, 21)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Male"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(276, 208)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(67, 21)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Female"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'txtContact
+        '
+        Me.txtContact.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtContact.Location = New System.Drawing.Point(195, 239)
+        Me.txtContact.MaxLength = 11
+        Me.txtContact.Name = "txtContact"
+        Me.txtContact.Size = New System.Drawing.Size(218, 25)
+        Me.txtContact.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(95, 242)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(75, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Contact# : "
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.Location = New System.Drawing.Point(195, 272)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(218, 25)
+        Me.txtEmail.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(96, 275)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(54, 17)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Email : "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(439, 150)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(143, 17)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Department Subject : "
+        '
+        'cbDepartmentSub
+        '
+        Me.cbDepartmentSub.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbDepartmentSub.FormattingEnabled = True
+        Me.cbDepartmentSub.Items.AddRange(New Object() {"Math", "Filipino", "Science", "English", "Art"})
+        Me.cbDepartmentSub.Location = New System.Drawing.Point(588, 145)
+        Me.cbDepartmentSub.Name = "cbDepartmentSub"
+        Me.cbDepartmentSub.Size = New System.Drawing.Size(260, 28)
+        Me.cbDepartmentSub.TabIndex = 16
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAddress.Location = New System.Drawing.Point(589, 185)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(259, 117)
+        Me.txtAddress.TabIndex = 18
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(439, 188)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(69, 17)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Address : "
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4, Me.ColumnHeader5, Me.ColumnHeader6, Me.ColumnHeader7})
+        Me.ListView1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListView1.HideSelection = False
+        Me.ListView1.Location = New System.Drawing.Point(43, 387)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(805, 237)
+        Me.ListView1.TabIndex = 19
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "TchrID"
+        Me.ColumnHeader1.Width = 65
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Fullname"
+        Me.ColumnHeader2.Width = 120
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Gender"
+        Me.ColumnHeader3.Width = 75
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Contact#"
+        Me.ColumnHeader4.Width = 95
+        '
+        'ColumnHeader5
+        '
+        Me.ColumnHeader5.Text = "Email"
+        Me.ColumnHeader5.Width = 175
+        '
+        'ColumnHeader6
+        '
+        Me.ColumnHeader6.Text = "DeptSubj"
+        Me.ColumnHeader6.Width = 85
+        '
+        'ColumnHeader7
+        '
+        Me.ColumnHeader7.Text = "Address"
+        Me.ColumnHeader7.Width = 216
+        '
+        'btnSave
+        '
+        Me.btnSave.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnSave.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.ForeColor = System.Drawing.Color.Black
+        Me.btnSave.Location = New System.Drawing.Point(148, 329)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(152, 40)
+        Me.btnSave.TabIndex = 20
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = False
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnUpdate.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.ForeColor = System.Drawing.Color.Black
+        Me.btnUpdate.Location = New System.Drawing.Point(306, 329)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(152, 40)
+        Me.btnUpdate.TabIndex = 21
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.ForeColor = System.Drawing.Color.Black
+        Me.btnClear.Location = New System.Drawing.Point(621, 329)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(152, 40)
+        Me.btnClear.TabIndex = 23
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnDelete
+        '
+        Me.btnDelete.BackColor = System.Drawing.Color.Goldenrod
+        Me.btnDelete.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDelete.ForeColor = System.Drawing.Color.Black
+        Me.btnDelete.Location = New System.Drawing.Point(463, 329)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(152, 40)
+        Me.btnDelete.TabIndex = 22
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(888, 653)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cbDepartmentSub)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtContact)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtFullname)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtFullname As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents txtContact As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents cbDepartmentSub As ComboBox
+    Friend WithEvents txtAddress As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents ColumnHeader5 As ColumnHeader
+    Friend WithEvents ColumnHeader6 As ColumnHeader
+    Friend WithEvents ColumnHeader7 As ColumnHeader
+End Class
---
+++ TeacherDepartment/TeacherDepartment/Form1.vb
@@ -0,0 +1,148 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+
+    Public conn As New MySqlConnection
+    Public cmd As New MySqlCommand
+    Public adapter As New MySqlDataAdapter
+    Public ds As New DataSet
+    Dim itemColl(999) As String
+    Dim genderBind As String
+
+    Dim gender As String
+
+    Sub openConn()
+        conn.ConnectionString = "server=localhost; username=root; password=; database=dbsample; port=3308"
+        conn.Open()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+
+        If txtFullname.Text = "" Or txtContact.Text = "" Or txtEmail.Text = "" Or txtAddress.Text = "" Or gender = "" Then
+            MsgBox("Please specify the blank inputs.")
+        Else
+            Try
+                openConn()
+                cmd.Connection = conn
+                cmd.CommandText = "insert into tblsampletwo (`Fullname`,`Gender`,`ContactNo`, `Email`, `DepartmentSubject`, `Address`) values ('" & txtFullname.Text & "','" & gender & "', '" & txtContact.Text & "', '" & txtEmail.Text & "', '" & cbDepartmentSub.Text & "', '" & txtAddress.Text & "' ) "
+                cmd.ExecuteNonQuery()
+                MsgBox("Successfuly saved")
+                clearInputs()
+                conn.Close()
+                loadListView()
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        gender = "Male"
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        gender = "Female"
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+
+        If txtFullname.Text = "" Or txtContact.Text = "" Or txtEmail.Text = "" Or txtAddress.Text = "" Or gender = "" Then
+            MsgBox("Please specify the blank inputs.")
+        Else
+            Try
+                openConn()
+                cmd.Connection = conn
+                cmd.CommandText = "update tblsampletwo set Fullname='" & txtFullname.Text & "', Gender ='" & gender & "', ContactNo='" & txtContact.Text & "', Email='" & txtEmail.Text & "', DepartmentSubject='" & cbDepartmentSub.Text & "', Address='" & txtAddress.Text & "' where TeacherID='" & txtID.Text & "'"
+                cmd.ExecuteNonQuery()
+                MsgBox("Successfuly updated")
+                clearInputs()
+                conn.Close()
+                loadListView()
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub loadListView()
+        Try
+            ListView1.Items.Clear()
+            openConn()
+            ds.Clear()
+            cmd.Connection = conn
+            cmd.CommandText = "Select * from tblsampletwo"
+            adapter.SelectCommand = cmd
+            adapter.Fill(ds, "table")
+            For r = 0 To ds.Tables(0).Rows.Count - 1
+                For c = 0 To ds.Tables(0).Columns.Count - 1
+                    itemColl(c) = ds.Tables(0).Rows(r)(c).ToString
+                Next
+                Dim listItems As New ListViewItem(itemColl)
+                ListView1.Items.Add(listItems)
+            Next
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadListView()
+    End Sub
+
+    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListView1.SelectedIndexChanged
+        If ListView1.SelectedItems.Count > 0 Then
+            txtID.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(0).Text
+            txtFullname.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(1).Text
+            gender = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(2).Text
+            genderBind = gender
+            If genderBind = "Male" Then
+                RadioButton1.Checked = True
+            ElseIf genderBind = "Female" Then
+                RadioButton2.Checked = True
+            End If
+            txtContact.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(3).Text
+            txtEmail.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(4).Text
+            cbDepartmentSub.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(5).Text
+            txtAddress.Text = ListView1.Items(ListView1.SelectedIndices(0)).SubItems(6).Text
+        End If
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+
+        If txtFullname.Text = "" Or txtContact.Text = "" Or txtEmail.Text = "" Or txtAddress.Text = "" Or gender = "" Then
+            MsgBox("Please specify the blank inputs.")
+        Else
+            Try
+                openConn()
+                cmd.Connection = conn
+                cmd.CommandText = "delete from tblsampletwo where TeacherID='" & txtID.Text & "'"
+                cmd.ExecuteNonQuery()
+                MsgBox("Successfuly deleted")
+                conn.Close()
+                clearInputs()
+                loadListView()
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+    End Sub
+
+    Sub clearInputs()
+
+        txtFullname.Clear()
+        txtAddress.Clear()
+        txtContact.Clear()
+        txtEmail.Clear()
+        txtID.Clear()
+        RadioButton1.Checked = False
+        RadioButton2.Checked = False
+        cbDepartmentSub.Text = ""
+
+    End Sub
+End Class
